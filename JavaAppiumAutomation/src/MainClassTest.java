f5751f7e906426e1df5e1a02eddadb9f857ae4b6
Nikishin Aleksandr <ex.kick@gmail.com>
2024-07-01 18:39:48 +0300

    first commit

---
+++ JavaAppiumAutomation/src/MainClassTest.java
@@ -0,0 +1,30 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainClassTest {
+
+    @Test
+    public void testGetLocalNumber() {
+        MainClass mainClass = new MainClass();
+        int localNumber = mainClass.getLocalNumber();
+        Assert.assertEquals("Метод getLocalNumber не возвращает число 14", 14, localNumber);
+    }
+
+    @Test
+    public void testGetClassNumber() {
+        MainClass mainClass = new MainClass();
+        int classNumber = mainClass.getClassNumber();
+        Assert.assertTrue("Метод getClassNumber не возвращает число больше 45", classNumber > 45);
+    }
+
+    @Test
+    public void testGetClassString() {
+        MainClass mainClass = new MainClass();
+        String classString = mainClass.getClassString();
+        boolean wordHello = classString.contains("Hello");
+        boolean wordhello = classString.contains("hello");
+
+        Assert.assertTrue("Метод getClassString не возвращает слова Hello или hello", wordHello || wordhello);
+    }
+}
